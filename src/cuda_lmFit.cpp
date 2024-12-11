b19120cdff4a785397b2f5a30bba0fb6b83eb596
bnjenner <bnjenner@ucdavis.edu>
2024-12-11 04:53:51 +0000

    clean and further validation

--- src/cuda_lmFit.cpp
+++ src/cuda_lmFit.cpp
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////
 /*
-	cuda_PCA
+	cuda_lmFit
 */
 /////////////////////////////////////////////////////////////////
 
@@ -28,14 +28,16 @@
 #include <cassert>
 #include <getopt.h>
 
+/////////////////////////////////////////////////////////////////
 void display_help(const char* program) {
-   std::cerr << "Description: cuda_lmFit is a tool used for fitting linear models utilizing GPU\n     computing. Currently implements, the least squares (standard or weighted) \n     algorithm. Does not filter or perform normalization. \n     Uses formula Y = Xb. Results are printed to standard out.\n\n"
+   std::cerr << "Description: cuda_lmFit is a tool used for fitting linear models utilizing GPU\n             computing. Currently implements, the least squares (standard or weighted) \n             algorithm. Does not filter or perform normalization. \n             Uses formula Y = Xb. Results are printed to standard out.\n\n"
              << "Usage: " << program << " [ OPTIONS ] Y_file X_file \n\n"
              << "Options:\n"
              << "  -h                Displays help message.\n"
              << "  -w weights_file   File containings wieghts. If not specified, least sqares is unweighted."
              << std::endl;
 }
+
 
 void argparse(int argc, char** argv, std::string &y_filename, std::string &x_filename, std::string &weights_file) {
 
@@ -74,23 +76,17 @@ void cuda_assert(cudaError_t status) {
    }
 }
 
-/*
-   
-   b = inv(transpose(X) * X) * transpose(X) * y
 
-*/
-
+/////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[]) {
 
 	std::cerr << "//cuda_lm\n";
 
-   // Arguments
    std::string y_filename;
    std::string x_filename;
    std::string weights_file;
 
    argparse(argc, argv, y_filename, x_filename, weights_file);
-
 
    if (y_filename.empty()) {
       display_help(argv[0]);
@@ -104,7 +100,7 @@ int main(int argc, char* argv[]) {
       exit(EXIT_FAILURE);
    }
 
-   std::cerr << "//Parsing files................";
+   std::cerr << "//Parsing files....................";
 
    /////////////////////////////////////////////////////////////////
    // Read in X
@@ -120,7 +116,6 @@ int main(int argc, char* argv[]) {
    std::cerr << "COMPLETE\n"; 
 
    // Get Dimensions and bytes
-   //   column major order on the transposed matrix = the same matrix lol  
    const unsigned long long int m = table.rows;
    const unsigned long long int n = table.cols;
    const unsigned long long int y_m = y_table.rows;
@@ -131,19 +126,18 @@ int main(int argc, char* argv[]) {
    const size_t weight_bytes = m * sizeof(double);
    const size_t y_bytes = y_m * y_n * sizeof(double);
 
-
-   // Print Table Stats
-   std::cerr << "    Rows: " << m << "\n"; 
-   std::cerr << "    Cols: " << n << "\n";  
-   std::cerr << "    Size: " << size << "\n"; 
-   std::cerr << "    Bytes: " << bytes << "\n"; 
-   std::cerr << "    Y Rows: " << y_m << "\n"; 
-   std::cerr << "    Y Cols: " << y_n << "\n";  
-   std::cerr << "    Y Size: " << y_m * y_n << "\n"; 
-   std::cerr << "    Y Bytes: " << y_bytes << "\n"; 
+   // Print Table Stats (for debugging purposes)
+   // std::cerr << "    Rows: " << m << "\n"; 
+   // std::cerr << "    Cols: " << n << "\n";  
+   // std::cerr << "    Size: " << size << "\n"; 
+   // std::cerr << "    Bytes: " << bytes << "\n"; 
+   // std::cerr << "    Y Rows: " << y_m << "\n"; 
+   // std::cerr << "    Y Cols: " << y_n << "\n";  
+   // std::cerr << "    Y Size: " << y_m * y_n << "\n"; 
+   // std::cerr << "    Y Bytes: " << y_bytes << "\n"; 
 
    /////////////////////////////////////////////////////////////////
-   // Create pointer to GPU
+   // Create pointers to GPU
    double *d_mat, *d_w_mat;
    double *d_y_mat, *d_w_y_mat;
    double *d_id_mat, *d_t_mat, *d_w_vec;
@@ -162,28 +156,27 @@ int main(int argc, char* argv[]) {
       weight_vec = get_one_vec(m);
    }
 
-
    ///////////////////////////////////////////////////////////////// 
-   // Create and initialize cuBLAS handle object
+   // Create and initialize cuBLAS handler objects
    cublasHandle_t handle;
    cublasCreate_v2(&handle);
    cublasStatus_t cublas_status;
    cudaError_t cuda_error;
 
    // Allocate GPU memory for vectors
-   std::cerr << "//Allocating GPU memory........";   
-   cuda_error = cudaMalloc((void**)&d_mat, bytes); cuda_assert(cuda_error);                           // Data Matrix
-   cuda_error = cudaMalloc((void**)&d_y_mat, y_bytes); cuda_assert(cuda_error);                       // Data Y Matrix
-   cuda_error = cudaMalloc((void**)&d_w_mat, bytes); cuda_assert(cuda_error);                         // Weighted Data Matrix
-   cuda_error = cudaMalloc((void**)&d_w_y_mat, y_bytes); cuda_assert(cuda_error);                     // Weighted Y Matrix
-   cuda_error = cudaMalloc((void**)&d_w_vec, weight_bytes); cuda_assert(cuda_error);                  // Identity Matrix
-   cuda_error = cudaMalloc((void**)&d_id_mat, id_bytes); cuda_assert(cuda_error);                     // Identity Matrix
-   cuda_error = cudaMalloc((void**)&d_t_mat, n * y_m * sizeof(double)); cuda_assert(cuda_error);      // Temporary Matrix
-   cuda_error = cudaMalloc((void**)&d_res_mat, n * y_m * sizeof(double)); cuda_assert(cuda_error);    // Result Matrix
-
- 
+   std::cerr << "//Allocating GPU memory............";   
+   cuda_error = cudaMalloc((void**)&d_mat, bytes); cuda_assert(cuda_error);                            // Data Matrix
+   cuda_error = cudaMalloc((void**)&d_y_mat, y_bytes); cuda_assert(cuda_error);                        // Data Y Matrix
+   cuda_error = cudaMalloc((void**)&d_w_mat, bytes); cuda_assert(cuda_error);                          // Weighted Data Matrix
+   cuda_error = cudaMalloc((void**)&d_w_y_mat, y_bytes); cuda_assert(cuda_error);                      // Weighted Y Matrix
+   cuda_error = cudaMalloc((void**)&d_w_vec, weight_bytes); cuda_assert(cuda_error);                   // Identity Matrix
+   cuda_error = cudaMalloc((void**)&d_id_mat, id_bytes); cuda_assert(cuda_error);                      // Identity Matrix
+   cuda_error = cudaMalloc((void**)&d_t_mat, n * y_m * sizeof(double)); cuda_assert(cuda_error);       // Temporary Matrix
+   cuda_error = cudaMalloc((void**)&d_res_mat, n * y_m * sizeof(double)); cuda_assert(cuda_error);     // Result Matrix
    std::cerr << "COMPLETE\n";  
-   std::cerr << "//Setting Matrices.............";  
+
+
+   std::cerr << "//Setting Matrices.................";  
    // Set Matrix and vectors (I am treating vector as matrices, deal it)
    cublas_status = cublasSetMatrix(1, 1, bytes, mat, 1, d_mat, 1);
    if (cublas_status != CUBLAS_STATUS_SUCCESS) { throw std::runtime_error("\n//ERROR: Could not set data matrix.\n");}
@@ -193,11 +186,12 @@ int main(int argc, char* argv[]) {
 
    cublas_status = cublasSetMatrix(1, 1, y_bytes, y_mat, 1, d_y_mat, 1); 
    if (cublas_status != CUBLAS_STATUS_SUCCESS) { throw std::runtime_error("\n//ERROR: Could not set normalization vector.\n");}
-
-
    std::cerr << "COMPLETE\n";  
-   std::cerr << "//Weighting Matrices...........";  
+
+
    ///////////////////////////////////////////////////////////////// 
+   std::cerr << "//Weighting Matrices...............";  
+
    // Weight X matrix 
    cublas_status = cublasDdgmm(handle, CUBLAS_SIDE_LEFT,
                                m, n, d_mat, m,
@@ -211,12 +205,13 @@ int main(int argc, char* argv[]) {
                                d_w_vec, 1,
                                d_w_y_mat, y_n);
    if (cublas_status != CUBLAS_STATUS_SUCCESS) { throw std::runtime_error("\n//ERROR: Could not weight matrix.\n");}
-
-
    std::cerr << "COMPLETE\n";  
-   std::cerr << "//Calculating X^t * X..........";  
+
+
    ///////////////////////////////////////////////////////////////// 
-   // Identity matrix 
+   std::cerr << "//Calculating X^t * X..............";  
+
+   // Calculate "Identity" matrix 
    double alpha = 1.0f;
    double beta = 0.0f;
    cublas_status = cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N,
@@ -225,20 +220,20 @@ int main(int argc, char* argv[]) {
                                d_w_mat, m, &beta,
                                d_id_mat, n);
    if (cublas_status != CUBLAS_STATUS_SUCCESS) { throw std::runtime_error("\n//ERROR: Could not calculate covariance matrix.\n");}
-
-
    std::cerr << "COMPLETE\n";  
-   std::cerr << "//Calculating Inverse..........";  
+
+
    ///////////////////////////////////////////////////////////////// 
-   // LU Decomposition
+   std::cerr << "//Calculating Inverse..............";  
+
+   // Set LU Decomposition Pointers
    double *d_inv, *d_work;
    int *d_pivot, *d_info;
    int lwork;
 
-   cuda_error = cudaMalloc((void**)&d_inv, n * n * sizeof(double)); cuda_assert(cuda_error);             // Weighted Data Matrix
-   cuda_error = cudaMalloc((void**)&d_pivot, n * sizeof(int)); cuda_assert(cuda_error);             // Weighted Data Matrix
-   cuda_error = cudaMalloc((void**)&d_info, sizeof(int)); cuda_assert(cuda_error);             // Weighted Data Matrix
-
+   cuda_error = cudaMalloc((void**)&d_inv, n * n * sizeof(double)); cuda_assert(cuda_error);       // Inverse Matrix
+   cuda_error = cudaMalloc((void**)&d_pivot, n * sizeof(int)); cuda_assert(cuda_error);            // Pivot Indices Matrix 
+   cuda_error = cudaMalloc((void**)&d_info, sizeof(int)); cuda_assert(cuda_error);                 // Device Info
 
    // Create cuSolver Handle
    cusolverDnHandle_t cusolver_handle;
@@ -258,7 +253,6 @@ int main(int argc, char* argv[]) {
                                       d_info);
    if (cusolver_status != CUSOLVER_STATUS_SUCCESS) { throw std::runtime_error("\n//ERROR: Could not perform LU decomposition.\n");}
 
-
    // Create identity matrix
    double *ones = get_one_vec(n);
    for (int i = 0; i < n; i++) {
@@ -272,37 +266,36 @@ int main(int argc, char* argv[]) {
                                       d_pivot, d_inv,
                                       n, d_info);
    if (cusolver_status != CUSOLVER_STATUS_SUCCESS) { throw std::runtime_error("\n//ERROR: Could not compute inverse.\n");}
-  
-
    std::cerr << "COMPLETE\n";  
-   std::cerr << "//Calculating X^t * Y..........";  
+
+
    ///////////////////////////////////////////////////////////////// 
+   std::cerr << "//Calculating X^t * Y..............";  
+  
    // Transpose X matrix * Weighted Y
-   alpha = 1.0f;
-   beta = 0.0f;
    cublas_status = cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N,
                                n, y_n, m, 
                                &alpha, d_mat, m,
                                d_w_y_mat, y_n, &beta,
                                d_t_mat, n);
    if (cublas_status != CUBLAS_STATUS_SUCCESS) { throw std::runtime_error("\n//ERROR: Could not calculate product of X^t and Y.\n");}
-
-
    std::cerr << "COMPLETE\n";  
-   std::cerr << "//Calculating Coefficients.....";  
+
+
    ///////////////////////////////////////////////////////////////// 
+   std::cerr << "//Calculating Coefficients.........";  
+
    // Inverse * (Transpose X matrix * Weighted Y)
-   alpha = 1.0f;
-   beta = 0.0f;
    cublas_status = cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N,
                                n, y_m, n, 
                                &alpha, d_inv, n,
                                d_t_mat, y_n, &beta,
                                d_res_mat, n);
    if (cublas_status != CUBLAS_STATUS_SUCCESS) { throw std::runtime_error("\n//ERROR: Could not calculate final weights.\n");}
-
-
    std::cerr << "COMPLETE\n";  
+
+   ///////////////////////////////////////////////////////////////// 
+   // Report results
 
    double *result_matrix = (double*)malloc(n * y_m * sizeof(double));
    cudaMemcpy(result_matrix, d_res_mat, n * y_m * sizeof(double), cudaMemcpyDeviceToHost);
@@ -311,9 +304,7 @@ int main(int argc, char* argv[]) {
    }
    std::cout << "\n";
    print_matrix(result_matrix, y_m, n);
-
-
   
-   std::cerr << "//SUCCESS........COMPLETED SUCCESSFULLY\n";  
+   std::cerr << "//PROGRAM COMPLETED SUCCESSFULLY!\n";  
    return 0;
    }
